c4b1175e4646f8f7ff24f8a0bb07a24d8a31cbb6
Ali Tambe <ali.tambe@gmail.com>
2021-12-04 21:22:52 +0530

    Created Modules

---
+++ Module 2/Import & Exports/mainmodule.js
@@ -0,0 +1,6 @@
+const arithModule = require("./arithmaticmodule");
+
+console.log("Add : "+arithModule.add(1,2));
+console.log("Sub : "+arithModule.sub(2,2));
+console.log("Mul : "+arithModule.mul(3,2));
+console.log("Div : "+arithModule.div(4,2));
